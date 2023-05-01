3e2ac06d9192c00b929e09f509133a93321a8928
Atrem Petrenko <64548375+Artem-ka-create@users.noreply.github.com>
2023-05-01 23:35:32 +0200

    presentation component finished

--- src/components/Presentation/PresentationComponent.js
+++ src/components/Presentation/PresentationComponent.js
@@ -4,22 +4,28 @@ import styles from './Presentation.module.css';
 
 function PresentationComponent(){
 
+    const statistik=[
+        {id:'1',numb:'3', name:'pocet rokov'},
+        {id:'2',numb:'18', name:'IT Firmy'},
+        {id:'3',numb:'14', name:'partnety'},
+        {id:'4',numb:'3', name:'organizatory'}
 
+    ]
 
 return(
     <div style={{background:'#EFD472'}}>
         <HeaderComponent/>
-        <img style={{width:'100%',display:"block"}}src='mountainsDemo.svg' alt='mountainsDemo'/>     
+        <h1 className={styles.projName }>IT Creative Lab</h1>
+        <img style={{width:'100%',display:"block"}}src='mountainsDemo.svg' alt='mountainsDemo'/>
+        
         <div className={styles.statisticBox}>
-            <div className={styles.statisticBox_item}>
-                ded
-            </div>
-            <div className={styles.statisticBox_item}>
-                ded
-            </div>
-            <div className={styles.statisticBox_item}>
-                ded
-            </div>
+        {statistik.map((item) => (
+
+        <div className={styles.statisticBox_item} key ={item.id} >
+            <div className={styles.number_box} >{item.numb}</div>
+            <div className={styles.stat_name_box} >{item.name}</div>
+        </div>
+      ))}
         </div> 
     </div>
     
