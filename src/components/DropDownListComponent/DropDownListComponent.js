386fa333cc47f14c2c465cb2907231d1c0fbb2aa
Atrem Petrenko <64548375+Artem-ka-create@users.noreply.github.com>
2023-05-08 15:34:21 +0200

    add accordion prime component

--- src/components/DropDownListComponent/DropDownListComponent.js
+++ src/components/DropDownListComponent/DropDownListComponent.js
@@ -1,5 +1,10 @@
 
 // import styles from './DropDownList.module.css';
+import { Accordion, AccordionTab } from 'primereact/accordion';
+import "primereact/resources/themes/lara-light-indigo/theme.css";     
+    
+//core
+import "primereact/resources/primereact.min.css";                     
 
 function DropDownListComponent(){
 
@@ -7,7 +12,32 @@ function DropDownListComponent(){
 
     return (
 
-       <h1>DROP DOWN LIST COMPONENT </h1>
+        <Accordion multiple activeIndex={[0]}>
+        <AccordionTab header="Header I">
+            <p className="m-0">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+                Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+                commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
+                Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </p>
+        </AccordionTab>
+        <AccordionTab header="Header II">
+            <p className="m-0">
+                Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa 
+                quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas 
+                sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. 
+                Consectetur, adipisci velit, sed quia non numquam eius modi.
+            </p>
+        </AccordionTab>
+        <AccordionTab header="Header III">
+            <p className="m-0">
+                At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti
+                quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt 
+                mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. 
+                Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus.
+            </p>
+        </AccordionTab>
+    </Accordion>
     )
 }
 
