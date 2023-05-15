f1a1a4655734d88bd1df4e180f62abfa4befb7a1
Atrem Petrenko <64548375+Artem-ka-create@users.noreply.github.com>
2023-05-15 16:13:54 +0200

    async yt

--- src/components/VideoPlayerComponent/VideoPlayerComponent.js
+++ src/components/VideoPlayerComponent/VideoPlayerComponent.js
@@ -1,12 +1,23 @@
-import React from 'react';
+import React , {useEffect}from 'react';
 import styles from './VideoPlayer.module.css'
 
 function VideoPlayer(){
 
+
+    useEffect(() => {
+        let frame = document.getElementById('yt');
+        console.log(frame);
+
+
+        let src = "https://www.youtube.com/embed/xNRJwmlRBNU" ;
+        setTimeout( ()=>  frame.setAttribute('src',src), 1000 );
+      }, []);
+
+
    
     return(
         <div className={styles.container}>
-            <iframe className={styles.responsiveIframe} src="https://www.youtube.com/embed/xNRJwmlRBNU" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen={true} ></iframe>
+            <iframe id='yt'  title='ytVideo' className={styles.responsiveIframe}  allowFullScreen={true} ></iframe>
         </div>
     )
 
